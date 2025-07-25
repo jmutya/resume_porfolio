48dc2e4977914b26b07f502c08776d3e65b679d0
jmutya <j.mutya.477708@umindanao.edu.ph>
2025-07-25 14:51:28 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio_project/',
-  plugins: [react(),tailwindcss()],
+  base: '/resume_porfolio/',
+  plugins: [react(), tailwindcss()],
 })
